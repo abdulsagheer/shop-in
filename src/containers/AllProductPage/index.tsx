34f34ffd4b1d386d94ea59c5b21e4223c9727549
Abdul Sagheer <73389096+abdulsagheer@users.noreply.github.com>
2022-08-13 23:28:00 +0530

    Added different pages based on routes

---
+++ src/containers/AllProductPage/index.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const AllProduct = (props: Props) => {
+  return (
+    <div>
+      <h1>AllProduct</h1>
+    </div>
+  );
+};
+
+export default AllProduct;
---
+++ src/containers/CheckoutPage/index.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const CheckoutPage = (props: Props) => {
+  return (
+    <div>
+      <h1>CheckoutPage</h1>
+    </div>
+  );
+};
+
+export default CheckoutPage;
